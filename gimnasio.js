42b2674e2d87384fa236f6e69011aec3219a0eee
EricPalMar <119602186+EricPalMar@users.noreply.github.com>
2024-05-09 00:51:24 -0500

    arrays
    lista de loss 50 productos

---
+++ gimnasio.js
@@ -0,0 +1,505 @@
+const accesoriosGimnasio = [
+    {
+      nombre: "Pesas de 5 kg",
+      categoria: "Pesas y Mancuernas",
+      precio: 87000,
+      color: "Negro",
+      tamaño: "5 kg",
+      material: "Hierro fundido",
+      codigo: "Acc001",
+      imagen: "../img/pesas5kg.jpeg"
+    },
+    {
+      nombre: "Tapete de Yoga",
+      categoria: "Accesorios de Yoga",
+      precio: 64000,
+      color: "Azul",
+      tamaño: "180cm x 60cm",
+      material: "Goma antideslizante",
+      codigo: "Acc002",
+      imagen: "../img/tapete.jpeg"
+    },
+    {
+      nombre: "Barra de dominadas",
+      categoria: "Equipos de Entrenamiento",
+      precio: 125000,
+      color: "Plateado",
+      tamaño: "120cm",
+      material: "Acero inoxidable",
+      codigo: "Acc003",
+      imagen: "../img/barradominada.jpeg"
+    },
+    {
+      nombre: "Banda de resistencia",
+      categoria: "Accesorios de Entrenamiento",
+      precio: 34000,
+      color: "Rojo",
+      tamaño: "1.5m",
+      material: "Látex",
+      codigo: "Acc004",
+      imagen: "../img/bandares.jpeg"
+    },
+    {
+      nombre: "Mancuernas Ajustables",
+      categoria: "Pesas y Mancuernas",
+      precio: 99900,
+      color: "Negro",
+      tamaño: "4 kg",
+      material: "Hierro fundido",
+      codigo: "Acc005",
+      imagen: "../img/mancuernaajustable.jpeg"
+    },
+    {
+      nombre: "Botella de agua deportiva",
+      categoria: "Accesorios de Hidratación",
+      precio: 29900,
+      color: "Transparente/Azul",
+      tamaño: "750ml",
+      material: "Plástico sin BPA",
+      codigo: "Acc006",
+      imagen: "../img/botella.jpeg"
+    },
+    {
+      nombre: "Rueda abdominal",
+      categoria: "Accesorios de Entrenamiento",
+      precio: 49900,
+      color: "Negro",
+      tamaño: "Un tamaño",
+      material: "Plástico resistente",
+      codigo: "Acc007",
+      imagen: "../img/ruedaabdominal.jpeg"
+    },
+    {
+      nombre: "Banda de compresión",
+      categoria: "Accesorios de Recuperación",
+      precio: 29900,
+      color: "Negro",
+      tamaño: "Un tamaño",
+      material: "Neopreno",
+      codigo: "Acc008",
+      imagen: "../img/bandacompres.jpeg"
+    },
+    {
+      nombre: "Bolsa de entrenamiento",
+      categoria: "Accesorios de Transporte",
+      precio: 59900,
+      color: "Negro/Verde",
+      tamaño: "40cm x 20cm x 20cm",
+      material: "Tela resistente",
+      codigo: "Acc009",
+      imagen: "../img/bolsa.jpeg"
+    },
+    {
+      nombre: "Escalera de agilidad",
+      categoria: "Accesorios de Entrenamiento Funcional",
+      precio: 39900,
+      color: "Amarillo/Negro",
+      tamaño: "5m",
+      material: "Plástico resistente",
+      codigo: "Acc010",
+      imagen: "../img/escalera.jpeg"
+    },
+    {
+      nombre: "Máquina de remo",
+      categoria: "Equipos Cardiovasculares",
+      precio: 1499000,
+      color: "Negro/Plata",
+      tamaño: "120cm x 60cm x 100cm",
+      material: "Acero y plástico",
+      codigo: "Acc011",
+      imagen: "../img/maquinaremo.jpeg"
+    },
+    {
+      nombre: "Manoplas de boxeo",
+      categoria: "Accesorios de Boxeo",
+      precio: 22900,
+      color: "Rojo/Negro",
+      tamaño: "Un tamaño",
+      material: "Cuero sintético",
+      codigo: "Acc012",
+      imagen: "../img/manoplas.jpeg"
+    },
+    {
+      nombre: "Colchoneta de entrenamiento",
+      categoria: "Accesorios de Entrenamiento",
+      precio: 75900,
+      color: "Azul",
+      tamaño: "180cm x 90cm",
+      material: "Espuma",
+      codigo: "Acc013",
+      imagen: "../img/colchoneta.jpeg"
+    },
+    {
+      nombre: "Gomas elásticas",
+      categoria: "Accesorios de Entrenamiento",
+      precio: 29900,
+      color: "Rojo/Verde/Amarillo",
+      tamaño: "1.5m",
+      material: "Látex",
+      codigo: "Acc014",
+      imagen: "../img/gomaelastica.jpeg"
+    },
+    {
+      nombre: "Cintas de suspensión",
+      categoria: "Accesorios de Entrenamiento",
+      precio: 79900,
+      color: "Negro/Azul",
+      tamaño: "2.5m",
+      material: "Nylon",
+      codigo: "Acc015",
+      imagen: "../img/cintasuspencion.jpeg"
+    },
+    {
+      nombre: "Cuerda para trepar",
+      categoria: "Accesorios de Entrenamiento Funcional",
+      precio: 119900,
+      color: "Marrón",
+      tamaño: "5m",
+      material: "Cuerda de cáñamo",
+      codigo: "Acc016",
+      imagen: "../img/cuerdatrepa.jpeg"
+    },
+    {
+      nombre: "Cinta métrica",
+      categoria: "Accesorios de Medición",
+      precio: 14900,
+      color: "Blanco",
+      tamaño: "3m",
+      material: "Plástico",
+      codigo: "Acc017",
+      imagen: "../img/cintametrica.jpeg"
+    },
+    {
+      nombre: "Escaladora de cardio",
+      categoria: "Equipos Cardiovasculares",
+      precio: 1899000,
+      color: "Negro/Plata",
+      tamaño: "150cm x 50cm x 150cm",
+      material: "Acero y plástico",
+      codigo: "Acc018",
+      imagen: "../img/escaladora.jpeg"
+    },
+    {
+      nombre: "Guantes de entrenamiento",
+      categoria: "Accesorios de Entrenamiento",
+      precio: 39900,
+      color: "Negro",
+      tamaño: "Un tamaño",
+      material: "Cuero sintético",
+      codigo: "Acc019",
+      imagen: "../img/guantes.jpeg"
+    },
+    {
+      nombre: "Porta botellas",
+      categoria: "Accesorios de Hidratación",
+      precio: 19900,
+      color: "Negro",
+      tamaño: "Un tamaño",
+      material: "Plástico",
+      codigo: "Acc020",
+      imagen: "../img/portabotella.jpeg"
+    },
+    {
+      nombre: "Colchón de yoga",
+      categoria: "Accesorios de Yoga",
+      precio: 92900,
+      color: "Verde",
+      tamaño: "180cm x 60cm",
+      material: "Gomaespuma",
+      codigo: "Acc021",
+      imagen: "../img/colchonyoga.jpeg"
+    },
+    {
+      nombre: "Cronómetro digital",
+      categoria: "Accesorios de Medición",
+      precio: 42900,
+      color: "Blanco/Negro",
+      tamaño: "Un tamaño",
+      material: "Plástico",
+      codigo: "Acc022",
+      imagen: "../img/cronometro.jpeg"
+    },
+    {
+      nombre: "Barras paralelas",
+      categoria: "Equipos de Entrenamiento",
+      precio: 249900,
+      color: "Negro",
+      tamaño: "100cm x 60cm x 100cm",
+      material: "Acero",
+      codigo: "Acc023",
+      imagen: "../img/barraparalela.jpeg"
+    },
+    {
+      nombre: "Cinturón de levantamiento de pesas",
+      categoria: "Accesorios de Levantamiento",
+      precio: 54900,
+      color: "Negro",
+      tamaño: "Un tamaño",
+      material: "Cuero",
+      codigo: "Acc024",
+      imagen: "../img/cinturon.jpeg"
+    },
+    {
+      nombre: "Banca de musculación",
+      categoria: "Equipos de Entrenamiento",
+      precio: 529900,
+      color: "Negro",
+      tamaño: "120cm x 30cm x 60cm",
+      material: "Acero y cuero sintético",
+      codigo: "Acc025",
+      imagen: "../img/bancamusculo.jpeg"
+    },
+    {
+      nombre: "Cinta de correr",
+      categoria: "Equipos Cardiovasculares",
+      precio: 1999900,
+      color: "Negro/Plateado",
+      tamaño: "180cm x 80cm x 120cm",
+      material: "Acero y plástico",
+      codigo: "Acc026",
+      imagen: "../img/cintacorrer.jpeg"
+    },
+    {
+      nombre: "Máquina de ejercicios multifuncional",
+      categoria: "Equipos de Entrenamiento",
+      precio: 1499900,
+      color: "Negro",
+      tamaño: "180cm x 120cm x 200cm",
+      material: "Acero",
+      codigo: "Acc027",
+      imagen: "../img/multifuncion.jpeg"
+    },
+    {
+      nombre: "Pesa rusa",
+      categoria: "Pesas y Mancuernas",
+      precio: 89900,
+      color: "Negro",
+      tamaño: "6 kg",
+      material: "Hierro fundido",
+      codigo: "Acc028",
+      imagen: "../img/pesarusa.jpeg"
+    },
+    {
+      nombre: "Guantes de boxeo",
+      categoria: "Accesorios de Boxeo",
+      precio: 99900,
+      color: "Negro/Rojo",
+      tamaño: "Un tamaño",
+      material: "Cuero sintético",
+      codigo: "Acc029",
+      imagen: "../img/guantesbox.jpeg"
+    },
+    {
+      nombre: "Comba de velocidad",
+      categoria: "Accesorios Cardiovasculares",
+      precio: 19900,
+      color: "Negro",
+      tamaño: "2.5m",
+      material: "Nylon",
+      codigo: "Acc030",
+      imagen: "../img/combavelocidad.jpeg"
+    },
+    {
+      nombre: "Máscara de entrenamiento",
+      categoria: "Accesorios de Entrenamiento",
+      precio: 39900,
+      color: "Negro",
+      tamaño: "Un tamaño",
+      material: "Plástico resistente",
+      codigo: "Acc031",
+      imagen: "../img/mascara.jpeg"
+    },
+    {
+      nombre: "Bolso para esterillas de yoga",
+      categoria: "Accesorios de Transporte",
+      precio: 29900,
+      color: "Negro",
+      tamaño: "70cm x 20cm",
+      material: "Tela resistente",
+      codigo: "Acc032",
+      imagen: "../img/bolsoesterilla.jpeg"
+    },
+    {
+      nombre: "Estación de entrenamiento",
+      categoria: "Equipos de Entrenamiento",
+      precio: 2299990,
+      color: "Negro",
+      tamaño: "200cm x 150cm x 220cm",
+      material: "Acero",
+      codigo: "Acc033",
+      imagen: "../img/estacion.jpeg"
+    },
+    {
+      nombre: "Bolso deportivo",
+      categoria: "Accesorios de Transporte",
+      precio: 39900,
+      color: "Gris",
+      tamaño: "50cm x 30cm x 30cm",
+      material: "Tela resistente",
+      codigo: "Acc034",
+      imagen: "../img/bolsodeportivo.jpeg"
+    },
+    {
+      nombre: "Bicicleta estática",
+      categoria: "Equipos Cardiovasculares",
+      precio: 3499990,
+      color: "Gris/Negro",
+      tamaño: "120cm x 60cm x 100cm",
+      material: "Acero y plástico",
+      codigo: "Acc035",
+      imagen: "../img/bicicleta.jpeg"
+    },
+    {
+      nombre: "Rollo de espuma para masajes",
+      categoria: "Accesorios de Recuperación",
+      precio: 44900,
+      color: "Negro",
+      tamaño: "30cm x 15cm",
+      material: "Espuma",
+      codigo: "Acc036",
+      imagen: "../img/rollo.jpeg"
+    },
+    {
+      nombre: "Balón medicinal",
+      categoria: "Accesorios de Entrenamiento Funcional",
+      precio: 89900,
+      color: "Negro",
+      tamaño: "3 kg",
+      material: "Piel sintética",
+      codigo: "Acc037",
+      imagen: "../img/balonmedicinal.jpeg"
+    },
+    {
+      nombre: "Set de bandas de resistencia",
+      categoria: "Accesorios de Entrenamiento",
+      precio: 79900,
+      color: "Negro/Rojo/Azul/Verde",
+      tamaño: "1.5m",
+      material: "Látex",
+      codigo: "Acc038",
+      imagen: "../img/setbandas.jpeg"
+    },
+    {
+      nombre: "Soga para saltar",
+      categoria: "Accesorios Cardiovasculares",
+      precio: 19900,
+      color: "Negro",
+      tamaño: "2.5m",
+      material: "Nylon",
+      codigo: "Acc039",
+      imagen: "../img/soga.jpeg"
+    },
+    {
+      nombre: "Rodillo de espuma",
+      categoria: "Accesorios de Recuperación",
+      precio: 34900,
+      color: "Azul",
+      tamaño: "30cm x 15cm",
+      material: "Espuma",
+      codigo: "Acc040",
+      imagen: "../img/rodillo.jpeg"
+    },
+    {
+      nombre: "Guantes de ciclismo",
+      categoria: "Accesorios de Ciclismo",
+      precio: 59900,
+      color: "Negro/Azul",
+      tamaño: "Un tamaño",
+      material: "Tejido transpirable",
+      codigo: "Acc041",
+      imagen: "../img/guanteciclismo.jpeg"
+    },
+    {
+      nombre: "Pelota de pilates",
+      categoria: "Accesorios de Pilates",
+      precio: 39900,
+      color: "Azul",
+      tamaño: "55cm",
+      material: "PVC",
+      codigo: "Acc042",
+      imagen: "../img/pelotapilate.jpeg"
+    },
+    {
+      nombre: "Soporte para saco de boxeo",
+      categoria: "Accesorios de Boxeo",
+      precio: 89900,
+      color: "Negro",
+      tamaño: "150cm",
+      material: "Acero",
+      codigo: "Acc043",
+      imagen: "../img/soportesaco.jpeg"
+    },
+    {
+      nombre: "Banco de abdominales",
+      categoria: "Equipos de Entrenamiento",
+      precio: 149900,
+      color: "Negro",
+      tamaño: "120cm x 30cm x 60cm",
+      material: "Acero",
+      codigo: "Acc044",
+      imagen: "../img/bancoabdominal.jpeg"
+    },
+    {
+      nombre: "Saco de boxeo",
+      categoria: "Equipos de Boxeo",
+      precio: 499900,
+      color: "Negro",
+      tamaño: "100cm x 30cm",
+      material: "Cuero sintético",
+      codigo: "Acc045",
+      imagen: "../img/sacobox.jpeg"
+    },
+    {
+      nombre: "Balón de fútbol",
+      categoria: "Accesorios Deportivos",
+      precio: 69900,
+      color: "Blanco/Negro",
+      tamaño: "5",
+      material: "Cuero sintético",
+      codigo: "Acc046",
+      imagen: "../img/futbol.jpeg"
+    },
+    {
+      nombre: "Pulsera de actividad",
+      categoria: "Accesorios de Seguimiento",
+      precio: 89900,
+      color: "Azul",
+      tamaño: "Un tamaño",
+      material: "Plástico y silicona",
+      codigo: "Acc047",
+      imagen: "../img/pulsera.jpeg"
+    },
+    {
+      nombre: "Auriculares deportivos",
+      categoria: "Accesorios de Audio",
+      precio: 79900,
+      color: "Negro",
+      tamaño: "Un tamaño",
+      material: "Plástico y goma",
+      codigo: "Acc048",
+      imagen: "../img/auriculares.jpeg"
+    },
+    {
+      nombre: "Tablero de dardos",
+      categoria: "Accesorios de Entretenimiento",
+      precio: 49900,
+      color: "Negro/Blanco",
+      tamaño: "40cm x 40cm",
+      material: "Plástico y metal",
+      codigo: "Acc049",
+      imagen: "../img/tablerodardos.jpeg"
+    },
+    {
+      nombre: "Guantes de portero",
+      categoria: "Accesorios de Fútbol",
+      precio: 59900,
+      color: "Verde/Negro",
+      tamaño: "Un tamaño",
+      material: "Látex",
+      codigo: "Acc050",
+      imagen: "../img/guanteportero.jpeg"
+    }
+  ];
+  
+  console.log(accesoriosGimnasio.length); 
+  
